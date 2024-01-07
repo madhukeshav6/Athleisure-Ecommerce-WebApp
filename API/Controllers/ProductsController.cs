92978db78b7c6f02a512e5a11352325dd13b1539
Madhu Keshav <madhukeshav6@gmail.com>
2024-01-07 18:24:36 +0530

    Implement API Error Handling
    Create consistent error response from the API
    Create Exception handler Middleware
    Improve the validation error responses

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -9,9 +9,7 @@ using API.DTOs;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+    public class ProductsController : BaseApiController
     {
         private readonly IGenericRepository<Product> _productsRepository;
         private readonly IGenericRepository<ProductBrand> _brandsRepository;

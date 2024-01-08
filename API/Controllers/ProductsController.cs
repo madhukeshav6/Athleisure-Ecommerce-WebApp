7ecd7a6ae25f3da9013ac8987cdd34c917b2d09d
Madhu Keshav <madhukeshav6@gmail.com>
2024-01-08 11:04:43 +0530

    Improve Swagger Documentation
    Improve Swagger Documentation
    Cleanup Program class by restructuring code and moving it to an extension method

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -6,6 +6,7 @@ using Core.Interfaces;
 using Core.Specifications;
 using AutoMapper;
 using API.DTOs;
+using API.Errors;
 
 namespace API.Controllers
 {
@@ -40,12 +41,19 @@ namespace API.Controllers
 
         [HttpGet]
         [Route("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
         {
 
             var spec = new ProductsWithTypesAndBrandsSpecification(id);
 
             var product = await _productsRepository.GetEntityWithSpec(spec);
+
+            if(product == null)
+            {
+                return NotFound(new ApiResponse(404));
+            }
 
             return Ok(_mapper.Map<Product, ProductToReturnDto>(product));
         }

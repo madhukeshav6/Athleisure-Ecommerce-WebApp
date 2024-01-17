a9c37725a415db22207de29366e5959acb58e637
Madhu Keshav <madhukeshav6@gmail.com>
2024-01-17 08:25:06 +0530

    Add the basket functionality: Angular
    Create the basket module, component and service.
    Implement the Add to cart functionality through:
    	- Product List component
    	- Product Item component
    	- Basket component
    Display the basket item count in the nav bar.
    Create and style the basket page.
    Create the order summary component and develop logic to display the summary.
    Add the increment, decrement functionality to add/remove basket items.
    Add the checkout module.

--- Client/src/app/shop/product-details/product-details.component.ts
+++ Client/src/app/shop/product-details/product-details.component.ts
@@ -4,6 +4,8 @@ import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
 import { Breadcrumb } from 'xng-breadcrumb/lib/types/breadcrumb';
 import { BreadcrumbService } from 'xng-breadcrumb';
+import { BasketService } from 'src/app/basket/basket.service';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-product-details',
@@ -13,12 +15,15 @@ import { BreadcrumbService } from 'xng-breadcrumb';
 export class ProductDetailsComponent implements OnInit{
 
   constructor(private shopService: ShopService,
-    private activatedRoute: ActivatedRoute,
-    private breadcrumbService: BreadcrumbService) {
+              private activatedRoute: ActivatedRoute,
+              private breadcrumbService: BreadcrumbService,
+              private basketService: BasketService) {
       this.breadcrumbService.set('@productDetails', '');
   }
 
   product?: Product;
+  quantity = 1;
+  quantityInBasket = 0;
 
   getProduct() {
     const id = this.activatedRoute.snapshot.paramMap.get('id');
@@ -27,10 +32,46 @@ export class ProductDetailsComponent implements OnInit{
         next: product => {
           this.product = product;
           this.breadcrumbService.set('@productDetails', product.name);
+          this.basketService.basketSource$.pipe(take(1)).subscribe({
+            next: basket => {
+              const item = basket?.items.find(x => x.id === +id);
+              if(item) {
+                this.quantity = item.quantity;
+                this.quantityInBasket = item.quantity;
+              }
+            }
+          });
         },
         error: error => console.log(error)
       })
     }
+  }
+
+  incrementQuantity() {
+    this.quantity++;
+  }
+
+  decrementQuantity() {
+    this.quantity--;
+  }
+
+  updateBasket() {
+    if(this.product) {
+      if(this.quantity > this.quantityInBasket) {
+        const quantityToAdd = this.quantity - this.quantityInBasket;
+        this.quantityInBasket += quantityToAdd;
+        this.basketService.addItemToBasket(this.product, quantityToAdd);
+      }
+      else {
+        const quantityToRemove = this.quantityInBasket - this.quantity;
+        this.quantityInBasket -= quantityToRemove;
+        this.basketService.removeItemFromBasket(this.product.id, quantityToRemove);
+      }
+    }
+  }
+
+  get buttonText() {
+    return this.quantityInBasket === 0 ? 'Add to basket' : 'Update basket';
   }
 
   ngOnInit(): void {
